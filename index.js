0275e48fc32016e5df4b7b814a6346ab773a04c2
NextCodigos <nebulanemesis376@gmail.com>
2025-01-16 14:03:33 +0100

    Varibles,comparadores lógicos y ternarios

---
+++ index.js
@@ -0,0 +1,19 @@
+let edad=18;
+const NOMBRE="Diego";
+let lenguaje="Javascript";
+let verdadero=true;
+
+let nota1=6;
+let nota2=4;
+let resultado=(nota1+nota2)/2;
+// let aprobado=resultado>=5;
+// let notaMaxima=resultado===10;
+
+// Operador ternario
+let aprobado=resultado>=5 ? "Has aprobado" : "Has suspendido";
+let notaMaxima=resultado===10 ? "Has sacado el maximo" : "No has sacado el maximo";
+// Para ver en consola pagina web escribo:
+// aprobado;
+// ("Has aprobado");
+// notaMaxima;
+// ("No has sacado el maximo"); 
